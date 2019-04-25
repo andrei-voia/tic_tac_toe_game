9a61fc70f996a6bd365452b74418d32d1cfb9262
Andrew <49621025+andrei-voia@users.noreply.github.com>
2019-04-25 23:45:22 +0300

    add code

---
+++ x si 0/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace x_si_0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(105, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(59, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // startToolStripMenuItem
+            // 
+            this.startToolStripMenuItem.Name = "startToolStripMenuItem";
+            this.startToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.startToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.startToolStripMenuItem.Text = "Start";
+            this.startToolStripMenuItem.Click += new System.EventHandler(this.startToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.E)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(738, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Dimensiune :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(301, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(301, 82);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(239, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Jucator 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(239, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Jucator 2";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(105, 86);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(59, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 89);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Partide :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(525, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 25);
+            this.label5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(636, 48);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(525, 86);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 24);
+            this.label7.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(636, 85);
+            this.label8.Name = "label8";
+            this.label8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label8.Size = new System.Drawing.Size(0, 24);
+            this.label8.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(738, 470);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ x si 0/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace x_si_0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Button[,] b = new Button[100, 100];
+        int stare = 0, n,contor,jucator1=0,jucator2=0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        int conditie()
+        {
+            for (int i = 1; i <= n; i++)
+                for (int j = 1; j <= n; j++)
+                {
+                    if (b[i, j].Enabled == false)
+                    {
+                        if(i<=n-2)  if (b[i, j].Text == b[i + 1, j].Text && b[i + 1, j].Text == b[i + 2, j].Text) return 1;     //in jos
+                        if(i>=3)    if (b[i, j].Text == b[i - 1, j].Text && b[i - 1, j].Text == b[i - 2, j].Text) return 1;     //in sus
+                        if(j<=n-2)  if (b[i, j].Text == b[i, j + 1].Text && b[i, j + 1].Text == b[i, j + 2].Text) return 1;     //la dreapta
+                        if(j>=3)    if (b[i, j].Text == b[i, j - 1].Text && b[i, j - 1].Text == b[i, j - 2].Text) return 1;     //la stanga
+                        if(i>=3&&j>=3)      if (b[i, j].Text == b[i - 1, j - 1].Text && b[i - 1, j - 1].Text == b[i - 2, j - 2].Text) return 1;     //diagonala stanga sus
+                        if(i>=3&&j<=n-2)    if (b[i, j].Text == b[i - 1, j + 1].Text && b[i - 1, j + 1].Text == b[i - 2, j + 2].Text) return 1;     //diagonala dreapta sus
+                        if(i<=n-2&&j<=n-2)  if (b[i, j].Text == b[i + 1, j + 1].Text && b[i + 1, j + 1].Text == b[i + 2, j + 2].Text) return 1;     //diagonala drepta jos
+                        if(i<=n-2&&j>=3)    if (b[i, j].Text == b[i + 1, j - 1].Text && b[i + 1, j - 1].Text == b[i + 2, j - 2].Text) return 1;     //diagonala stanga jos
+                    }
+                }
+            return 0;
+        }
+
+        private void b_Click(object sender, EventArgs e)
+        {
+            if (stare % 2 == 0)
+            {
+                ((Button)sender).Text = "X";
+                ((Button)sender).Enabled = false;
+            }
+            else
+            {
+                ((Button)sender).Text = "O";
+                 ((Button)sender).Enabled = false;
+            }
+            stare++;
+            if (conditie() == 1)
+            {
+                char k; if (stare % 2 == 0)
+                {
+                    jucator1++;
+                    label8.Text = Convert.ToString(jucator1);
+                    k = 'O';
+                    MessageBox.Show("Jucatorul " + textBox3.Text + " care a jucat cu piesa " + k + " a castigat aceasta tura !");
+                    stare = 0;
+                }
+
+                else
+                {
+                    jucator2++;
+                    label7.Text = Convert.ToString(jucator2);
+                    k = 'X';
+                    MessageBox.Show("Jucatorul " + textBox2.Text + " care a jucat cu piesa " + k + " a castigat aceasta tura !");
+                    stare = 0;
+                }
+
+                if(jucator1==contor)
+                {
+                    label8.Text = Convert.ToString(jucator1);
+                    MessageBox.Show("Jucatorul " + textBox3.Text + " a castigat partida !");
+                    Application.Exit();
+                }
+
+                if (jucator2 == contor)
+                {
+                    label7.Text = Convert.ToString(jucator2);
+                    MessageBox.Show("Jucatorul " + textBox2.Text + " a castigat partida !");
+                    Application.Exit();
+                }
+
+                for (int i = 1; i <= n; i++)
+                    for (int j = 1; j <= n; j++)
+                    {
+                        b[i, j].Enabled = true;
+                        b[i, j].Text = null;
+                    }
+
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        private void startToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                for (int i = 1; i <= n; i++)
+                    for (int j = 1; j <= n; j++)
+                        Controls.Remove(b[i, j]);
+
+                        n = Convert.ToInt32(textBox1.Text);
+                contor = Convert.ToInt32(textBox4.Text);
+                label5.Text = textBox2.Text;
+                label6.Text = textBox3.Text;
+                label7.Text = label8.Text = "0";
+
+            
+                for (int i = 1; i <= n; i++)
+                    for (int j = 1; j <= n; j++)
+                    {
+                        b[i, j] = new Button();
+                        b[i, j].Top = i * 50 + 80;
+                        b[i, j].Left = j * 50;
+                        b[i, j].Width = 50;
+                        b[i, j].Height = 50;
+                        b[i, j].Click += new EventHandler(b_Click);
+                        Controls.Add(b[i, j]);
+                    }
+
+            }
+        }
+    }
+}
